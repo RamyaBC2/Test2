0c2e77369941f9f3c6ad22a95be481f72be552b9
admin <admin@DESKTOP-LLREKF9>
2022-07-27 20:54:44 +0530

    Test2

---
+++ Java/src/large_number.java
@@ -0,0 +1,16 @@
+
+public class large_number {
+
+	public static void main(String[] args) {
+		int[] A={12,24,34,56,78,99,96};
+		for(int i=0;i<A.length;i++)
+		{
+			if(A[i]>96)
+			{
+				System.out.println(A[i]);
+			}
+		}
+
+	}
+
+}
